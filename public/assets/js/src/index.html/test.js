0c4123f5d40c97ea99b2bf07395aed54adc8f52b
IchBinHanz <nilssimons707@gmail.com>
2023-09-29 11:18:27 +0200

    1.*

---
+++ public/assets/js/src/index.html/test.js
@@ -0,0 +1,42 @@
+let point1 = null;
+let point2 = null;
+
+function setup() {
+  createCanvas(1024, 1024);
+}
+
+function draw() {
+  // background(220);
+
+  // Draw the first point if it exists
+  if (point1 !== null) {
+    fill(255, 0, 0);
+    ellipse(point1.x, point1.y, 10, 10);
+  }
+
+  // Draw the second point if it exists
+  if (point2 !== null) {
+    fill(0, 0, 255);
+    ellipse(point2.x, point2.y, 10, 10);
+
+    // Draw a line between the two points
+    stroke(200, 212, 28);
+    line(point1.x, point1.y, point2.x, point2.y);
+  }
+}
+
+function mouseClicked() {
+  // On mouse click, set the first point if it's not set yet
+  if (point1 === null) {
+    point1 = createVector(mouseX, mouseY);
+    console.assert(mouseX, mouseY);
+  } else if (point2 === null) {
+    // Set the second point if the first point is already set
+    point2 = createVector(mouseX, mouseY);
+    console.assert(mouseX, mouseY);
+  } else {
+    // If both points are already set, reset them on the next click
+    point1 = null;
+    point2 = null;
+  }
+}
---
+++ public/assets/js/utils/convAlgo.js
@@ -0,0 +1,6 @@
+function convXY(x1, y1, x2, y2) {
+
+    
+
+    return (x1, y1, x2, y2)
+}
